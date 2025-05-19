6648d91d1f41da4ffe5a743010d17d6a7f89b7ba
ahmed <ahmed@DESKTOP-NPGEGAF>
2025-05-19 23:56:37 +0300

    Register Services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
+using BlogApp.Models.BlogApp.Models;
+using BlogApp.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Hangfire.Redis.StackExchange;
+using Hangfire;
 
 namespace BlogApp
 {
@@ -7,25 +15,39 @@ namespace BlogApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // Add services
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<BlogContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+            builder.Services.AddAuthorization();
+            builder.Services.AddHangfire(config => config
+                .UseRedisStorage(builder.Configuration.GetConnectionString("Redis"), new RedisStorageOptions()));
+            builder.Services.AddHangfireServer();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IPostService, PostService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            // Configure middleware
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
+            app.UseHangfireDashboard();
             app.MapControllers();
 
             app.Run();
